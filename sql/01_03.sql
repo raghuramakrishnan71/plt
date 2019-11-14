d4bdf806a3cf801c7bcfe0f700979b12906e84b8
Raghu Ramakrishnan <raghuramakrishnan71@gmail.com>
2019-11-14 22:01:32 +0530

    Delete 01_03.sql

--- sql/01_03.sql
+++
@@ -1,8 +0,0 @@
-#standardSQL
-# 03: Percentage of select categories
-SELECT 
-  COUNTIF(category IN ('Information Technology', 'News and Media', 'Business', 'Shopping', 'Education', 'Entertainment', 'Finance and Banking', 'Search Engines and Portals', 'Travel', 'Government and Legal Organizations')) AS cnt,
-  COUNT(category) AS total,
-  ROUND(COUNTIF(category IN ('Information Technology', 'News and Media', 'Business', 'Shopping', 'Education', 'Entertainment', 'Finance and Banking', 'Search Engines and Portals', 'Travel', 'Government and Legal Organizations')) * 100/ COUNT(category), 2) AS pct
-FROM 
-  `httparchiveext.urls.20170315_urls`
